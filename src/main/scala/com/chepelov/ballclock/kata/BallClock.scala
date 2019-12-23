f97d3a90766c0fb38eecd7c5d1a6ddf93fb1d8b7
Cyrille Chépélov <cch@transparencyrights.com>
2019-12-23 14:44:04 +0100

    Much faster, still wrong

--- src/main/scala/com/chepelov/ballclock/kata/BallClock.scala
+++ src/main/scala/com/chepelov/ballclock/kata/BallClock.scala
@@ -7,6 +7,7 @@ import cats.Foldable
 import cats.instances.order
 import cats.implicits._
 import cats.Monoid
+import com.chepelov.ballclock.kata
 import zio.stream.ZStream
 
 import scala.annotation.tailrec
@@ -35,8 +36,12 @@ sealed abstract class BallClock {
       val maxExistingBall = (end :: stages).flatMap(_.contents.to[List].maximumOption).maximumOption
 
       val newBalls = (maxExistingBall.map(_.id).getOrElse(0) until ballCount).map(num => Ball(num + 1))
-      val newEnd = BallClock.feedBallsIntoMain(end, newBalls.to[List])
-      UIO(BallClock.Runnable(stages = this.stages, end = newEnd))
+
+      for {
+        newEnd <- BallClock.feedBallsIntoMain(end, newBalls.to[List])
+      } yield {
+        BallClock.Runnable(stages = this.stages, end = newEnd)
+      }
     }
   }
 }
@@ -44,37 +49,42 @@ sealed abstract class BallClock {
 
 object BallClock {
 
-  private def feedBallsIntoMain(end: Accumulator.Static, toReceive: List[Ball]): Accumulator.Static.NonEmpty =
+  private def feedBallsIntoMain(end: Accumulator.Static, toReceive: List[Ball]): IO[KataError.Empty.type, Accumulator.Static.NonEmpty] =
     end match {
       case nonEmptyEnd: Accumulator.Static.NonEmpty =>
-        toReceive match {
-          case Nil =>
-            nonEmptyEnd
-          case single :: Nil =>
-            val (newEndState, None) = nonEmptyEnd.receive(single)
-            newEndState
-
-          case _ =>
-            toReceive.foldLeft(nonEmptyEnd) { case (endState, ball) =>
-              val (newEndState, None) = endState.receive(ball)
+        UIO {
+          toReceive match {
+            case Nil =>
+              nonEmptyEnd
+            case single :: Nil =>
+              val (newEndState, None) = nonEmptyEnd.receive(single)
               newEndState
-            }
+
+            case _ =>
+              toReceive.foldLeft(nonEmptyEnd) { case (endState, ball) =>
+                val (newEndState, None) = endState.receive(ball)
+                newEndState
+              }
+          }
         }
       case emptyEnd: Accumulator.Static.Empty =>
         toReceive match {
           case Nil =>
-            throw new IllegalStateException("we should never reach here — catastrophic failure") // we could put that into the monad if there was an errorable monadic Schedule.unfoldM
+            IO.fail(KataError.Empty)
+
           case singleBall :: Nil =>
             val (newEndState, None) = emptyEnd.receive(singleBall)
-            newEndState
+            UIO(newEndState)
 
           case firstBall :: otherBalls =>
             val (firstNewEndState, None) = emptyEnd.receive(firstBall)
 
-            otherBalls.foldLeft(firstNewEndState) { case (endState, ball) =>
+            val result = otherBalls.foldLeft(firstNewEndState) { case (endState, ball) =>
               val (newEndState, None) = endState.receive(ball)
               newEndState
             }
+
+            UIO(result)
         }
     }
 
@@ -83,8 +93,7 @@ object BallClock {
   @tailrec
   private def stageMotion(stages: List[Accumulator.Tilting], maybeBall: Option[Ball],
                           end: Accumulator.Static,
-                          towardsEnd: Seq[Ball] = Nil, newStagesReversed: List[Accumulator.Tilting]):
-  (List[Accumulator.Tilting], Accumulator.Static.NonEmpty) = {
+                          towardsEnd: Seq[Ball] = Nil, newStagesReversed: List[Accumulator.Tilting]): IO[KataError, (List[Accumulator.Tilting], Accumulator.Static.NonEmpty)] = {
     stages match {
       case Nil =>
         // everything that might be moving, is moving. Time to collect every ball hurtling towards
@@ -94,8 +103,11 @@ object BallClock {
           case None => towardsEnd.to[List]
         }
 
-        val newEnd = feedBallsIntoMain(end, toReceive)
-        (newStagesReversed.reverse, newEnd)
+        for {
+          newEnd <- feedBallsIntoMain(end, toReceive)
+        } yield {
+          (newStagesReversed.reverse, newEnd)
+        }
 
       case currentStage :: otherStages =>
         maybeBall match {
@@ -128,14 +140,29 @@ object BallClock {
   private case class NotRunnable(stages: List[Accumulator.Tilting], end: Accumulator.Static) extends BallClock {
   }
 
+  private def runUntilLikeStartOrError[R, E, A](start: A, stepIO: A => ZIO[R, E, A], stopCondition: A => Boolean) =
+    Schedule
+      .unfoldM(stepIO(start).either) {
+        case error @ Left(_) => UIO(error)
+        case Right(state) => stepIO(state).either
+      }
+      .untilOutput {
+        case Left(_) => true
+        case Right(state) => stopCondition(state)
+      }
+
+
   case class Runnable(stages: List[Accumulator.Tilting], end: Accumulator.Static.NonEmpty) extends BallClock {
-    def countUntilCycle(): UIO[Duration] = {
+    def countUntilCycle(): IO[KataError, Duration] = {
 
 
-      val schedule = (Schedule.unfoldM(this.tick)(_.tick).untilOutput(_ == this) && Schedule.forever)
+      val schedule = runUntilLikeStartOrError(this, (_: Runnable).tick, (_:Runnable) == this) && Schedule.forever
 
       val result = ZIO.unit.repeat(schedule)
-        .map { case (_, minutes) => Duration.ofMinutes(minutes) }
+        .flatMap {
+          case (Left(error), _) => IO.fail(error)
+          case (Right(_), minutes) => IO.succeed(Duration.ofMinutes(minutes))
+        }
 
       result
     }
@@ -145,13 +172,15 @@ object BallClock {
      * Perform all the motions related to one motion of the rotating arm (pick a ball,
      * @return
      */
-    def tick: IO[Nothing, BallClock.Runnable] = {
+    def tick: IO[KataError, BallClock.Runnable] = {
 
       for {
         first <- end.takeFirst
+        (movingBall, initialNewEnd) = first
+
+        motion <- BallClock.stageMotion(stages, Some(movingBall), initialNewEnd, Nil, Nil)
+        (newStages, newEnd) = motion
       } yield {
-        val (movingBall, initialNewEnd) = first
-        val (newStages, newEnd) = BallClock.stageMotion(stages, Some(movingBall), initialNewEnd, Nil, Nil)
         this.copy(stages = newStages, end = newEnd)
       }
     }
